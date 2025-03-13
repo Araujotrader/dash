0f4a266a886721eb6e6835ea2ae94ceea3c50983
Thiago <araujo.linux@gmail.com>
2025-03-13 16:28:26 -0300

    modified:   teste.py

--- teste.py
+++ teste.py
@@ -1,16 +1,16 @@
 import pandas as pd
-#import potly
+import potly
 
 #dados = pd.read_csv('teste.csv')
 
 dados = pd.read_csv('teste.csv')
 
-#print(dados.describe())
-#print(dados.info())
+print(dados.describe())
+print(dados.info())
 
 print(dados.head(10))
 
-#print(dados[(dados['continent'] == 'Americas')])
+print(dados[(dados['continent'] == 'Americas')])
 
 
 
